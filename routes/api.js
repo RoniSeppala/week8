7b240d7396254d6efc650cdf4328188dc7a83817
Roni Seppälä <roni.o.seppala@gmail.com>
2024-01-15 20:59:10 +0200

    fix

--- routes/api.js
+++ routes/api.js
@@ -4,25 +4,28 @@ const bcrypt = require('bcryptjs')
 const User = require("../models/User");
 
 router.post("/user/register",async (req,res) => {
+    try {
+        const existingUser = await User.findOne({email: req.body.email});
 
-    const existingUser = await User.findOne({email: req.body.email});
-
-    if (existingUser) {
-        console.log("usererror")
-        return res.status(403).json({email: "Email is allready in use"});
-    } else {
-        bcrypt.genSalt(10, (err,salt) => {
-            bcrypt.hash(req.body.password, salt, (err,hash) => {
-                if(err) throw err;
-                User.create(
-                    {
-                        email: req.body.email,
-                        password: hash
-                    }
-                )
+        if (existingUser) {
+            console.log("usererror")
+            return res.status(403).json({email: "Email is allready in use"});
+        } else {
+            bcrypt.genSalt(10, (err,salt) => {
+                bcrypt.hash(req.body.password, salt, (err,hash) => {
+                    if(err) throw err;
+                    User.create(
+                        {
+                            email: req.body.email,
+                            password: hash
+                        }
+                    )
+                })
+                res.send("ok").status(200)
             })
-            res.send("ok").status(200)
-        })
+        }
+    } catch (error) {
+        console.log("error")
     }
 })
 
